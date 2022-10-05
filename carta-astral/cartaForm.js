50ec5c62bd045c792813859a7b43babd9eeab736
Brian Suárez <suarezbrianalan@gmail.com>
2022-10-05 13:40:56 -0300

    add sidebar

--- carta-astral/cartaForm.js
+++ carta-astral/cartaForm.js
@@ -94,3 +94,19 @@ form.addEventListener('submit', validarFormulario);
         alert("funciono");
     }
   }
+
+// navbar sidebar
+function openNav() {
+
+  if(parseInt(document.getElementById("mySidebar").style.width) > 0)
+  {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("header").style.marginLeft = "0";
+  }
+  else
+  {
+    document.getElementById("mySidebar").style.width = "20%";
+    document.getElementById("header").style.marginLeft = "20%";
+  }
+
+}

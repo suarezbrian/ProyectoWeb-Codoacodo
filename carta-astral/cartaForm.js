dcfef307574f826e8e5b848a065add760a3607cf
Brian Suárez <suarezbrianalan@gmail.com>
2022-10-29 20:10:11 -0300

    add iframe

--- carta-astral/cartaForm.js
+++ carta-astral/cartaForm.js
@@ -63,7 +63,7 @@ form.addEventListener('submit', validarFormulario);
     }
     else if(nombre.length > 10)
     {
-      sweetError('El nombre es demasiado largo.');
+      sweetError('El nombre es   largo.');
       return;
     }
 
